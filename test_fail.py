f847dd4c896e68233f7c3506df5d9c292fa4b65a
Cam Poole <cameron.poole@fdbhealth.com>
2024-03-13 09:59:34 +0000

    should fail

--- test_fail.py
+++ test_fail.py
@@ -3,3 +3,5 @@
 connString = {
   "Database.Connection": "Server=10.246.8.24;Database=MEDK_new;uid=IntlReader;Password=vawKQGIRBxrK5Ok6CyNm;TrustServerCertificate=true;"
 }
+
+adafruit_io_key={get_key}
